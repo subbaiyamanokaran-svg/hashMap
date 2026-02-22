ea89334b1799cf08980775e951085012fd88eaa1
subbaiyamanokaran-svg <subbaiya.manokaran@gmail.com>
2026-02-22 19:38:55 +0530

    twosum first commit

---
+++ twosum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class twosum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        int[] result = twoSum(arr, target);
+        System.out.println(Arrays.toString(result));
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
